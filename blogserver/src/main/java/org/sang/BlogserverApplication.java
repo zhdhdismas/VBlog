e79bb4cd80b28d600a5d7b3dc78d03dc134679c8
shz <shz200273@163.com>
2022-10-07 20:54:05 +0800

    asdasd

--- blogserver/src/main/java/org/sang/BlogserverApplication.java
+++ blogserver/src/main/java/org/sang/BlogserverApplication.java
@@ -11,5 +11,6 @@ public class BlogserverApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogserverApplication.class, args);
         System.out.println("why");
+        System.out.println("why");
     }
 }
